713f1904242fd68bb11e2417b8c1df19ac8d25e6
AudioByAri <audiobyari@gmail.com>
2018-09-24 01:25:28 -0700

    Dynamic coloring

--- sketch.js
+++ sketch.js
@@ -1,7 +1,34 @@
+
+
+var rgbColor = {
+  redFill : 0,
+  greenFill : 0,
+  blueFill : 0
+}
+
 function setup() {
+  createCanvas(600,600);
   
 }
 
 function draw() {
   
+  background(0);
+  
+  ellipseMode(CENTER);
+  fill('#5F9F9F');
+  ellipse(mouseX,mouseY,25,25);
+  
+  //redSaturation = map(mouseX,0,600,0,255);
+  
+  var r = rgbColor.redFill;
+  var g = rgbColor.greenFill;
+  var b = rgbColor.blueFill;
+  
+  r = map(mouseX,0,width,0,255);
+  g = map(mouseY,width,0,0,255);
+  //b = map()
+  rectMode(CENTER);
+  fill(r, g, b);
+  rect(300,300,200,200);
 }
