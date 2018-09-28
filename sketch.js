434393116240d743589feed57aad3a869475d00e
AudioByAri <audiobyari@gmail.com>
2018-09-27 22:32:20 -0700

    added tower element

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let player = [];
+var tower_basic = [];
 
 
 function setup() {
@@ -9,8 +10,8 @@ function setup() {
   // var topBorder = line(0,height,width,height);//top line
   // var bottomBorder = line(0,0,width,0) //bottom line
 
-  let startX = 100,
-      startY = 300,
+  let playerStartX = 100,
+      playerStartY = 300,
       playerW = 50,
       playerH = 50;
       playerRGB_R = 0;
@@ -18,17 +19,23 @@ function setup() {
       playerRGB_B = 0;
       
   for(let i = 0; i < 1; i++) {
-    player[i] = new shooter_class(startX, startY, playerW, playerH, playerRGB_R, playerRGB_G, playerRGB_B);
+    player[i] = new shooter_class(playerStartX, playerStartY, playerW, playerH, playerRGB_R, playerRGB_G, playerRGB_B);
+  }
+
+  let towerStartX = 300,
+      towerStartY = 200,
+      towerW = 50,
+      towerH = 50,
+      towerRGB_R = 255,
+      towerRGB_G = 0,
+      towerRGB_B = 0;
+
+  for(let i = 0; i < 1; i++) {
+    tower_basic[i] = new shooter_class(towerStartX, towerStartY, towerW, towerH, towerRGB_R, towerRGB_G, towerRGB_G);
   }
 
 }
 
-function draw() {
-  background(0);
-  for(let i = 0; i < player.length; i++) {
-    player[i].show();
-  }
-}
 
 
 
@@ -37,17 +44,49 @@ function keyPressed () {
 
   for(let i = 0; i < player.length; i++) {
     if (keyCode === RIGHT_ARROW) {
-      player[i].x += 25;
+      player[i].x += 50;
     } else if(keyCode === LEFT_ARROW) {
-      player[i].x -= 25;
+      player[i].x -= 50;
     } else if(keyCode === UP_ARROW) {
-      player[i].y -= 25;
+      player[i].y -= 50;
     } else if(keyCode === DOWN_ARROW) {
-      player[i].y += 25;
+      player[i].y += 50;
     }
   }
 
+  // for(let i = 0; i < player.length; i++) {
+
+  //   let eliminate = 0;
+  //   if (tower_basic[i].kill(enter)) {
+  //     eliminate = 1;
+  //   }
+
+  // }
 }
+
+
+
+
+
+function draw() {
+
+  background(0);
+
+  for(let i = 0; i < player.length; i++) {
+    player[i].show();
+  }
+
+  for(let i = 0; i < tower_basic.length; i++) {
+    tower_basic[i].show();
+  }
+
+}
+
+
+
+
+
+
 
 
 
@@ -63,10 +102,14 @@ class shooter_class {
     this.g = RGB_G;
     this.b = RGB_B;
   }
+
+  
+
   show() {
     fill(this.r, this.g, this.b);
     rect(this.x, this.y, this.w, this.h);
   }
+
 }
 
 
